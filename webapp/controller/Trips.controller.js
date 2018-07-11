6e062553f6b099b2cfd33ea2f360f2f012c15d7f
Elena <elena.makarenko@sap.com>
2018-07-11 07:49:48 +0200

    Use DatePicker

--- webapp/controller/Trips.controller.js
+++ webapp/controller/Trips.controller.js
@@ -3,9 +3,8 @@ sap.ui.define([
 		"ui5/awc/demo/controller/BaseController",
 		"sap/ui/model/json/JSONModel",
 		"ui5/awc/demo/model/formatter",
-		"ui5/awc/demo/controls/AwcProfileManager",
-		"sap/ui/unified/DateRange"
-	], function (BaseController, JSONModel, formatter, AwcProfileStore, DateRange) {
+		"ui5/awc/demo/controls/AwcProfileManager"
+	], function (BaseController, JSONModel, formatter, AwcProfileStore) {
 		"use strict";
 
 		return BaseController.extend("ui5.awc.demo.controller.Trips", {
@@ -24,29 +23,14 @@ sap.ui.define([
 			/* =========================================================== */
 			/* event handlers                                              */
 			/* =========================================================== */
-			onDaysLeftChange: function (oEvent) {
-				var fValue = -oEvent.getParameter("value");
-				if(fValue <= 1) {
-					AwcProfileStore.updateProfile({"details": "full"});
-				}
-				else if(fValue < 10) {
-					AwcProfileStore.updateProfile({"details": "mid"});
-				}
-				else {
-					AwcProfileStore.updateProfile({"details": "short"});
-				}
 
-			},
+			onDateChanged: function(oEvent) {
+				var oValue = oEvent.getParameter("value"),
+					oSelectedDate = new Date(oValue),
+					oTripDate = this._getTripDate();
 
-			onDateSelected: function(oEvent) {
-				var oCalendar = oEvent.oSource,
-					oSelectedDateRange = oCalendar.getSelectedDates()[0],
-					oSelectedDate = new Date(oSelectedDateRange.getStartDate());
+				var iDiffDays = formatter.getDateDiffDays(oSelectedDate,oTripDate)
 
-				var oTripDetails = this.getOwnerComponent().getModel("tripDetails");
-				var oTripDate = oTripDetails.getProperty("/beginDate");
-
-				var iDiffDays = formatter.getDateDiffDays(oSelectedDate, new Date(oTripDate))
 				if(iDiffDays <= 1) {
 					AwcProfileStore.updateProfile({"details": "full"});
 				}
@@ -75,9 +59,17 @@ sap.ui.define([
 				var oTripDetails = this.getOwnerComponent().getModel("tripDetails");
 				var sTripDate = oTripDetails.getProperty("/beginDate");
 				var oTripDate = new Date(sTripDate);
-				oTripDate.setDate(oTripDate.getDate() - 20);
-				this.byId("calendar").addSelectedDate(new DateRange({startDate: oTripDate}));
+				var oCurrentDate = new Date();
+				oCurrentDate.setDate(oTripDate.getDate() - 20);
+				var oDatePicker = this.byId("datePicker");
+				oDatePicker.setDateValue(oCurrentDate);
 				//this.byId("calendar").displayDate(oTripDate);
+			},
+
+			_getTripDate: function() {
+				var oTripDetails = this.getOwnerComponent().getModel("tripDetails");
+				var oTripDate = oTripDetails.getProperty("/beginDate");
+				return new Date(oTripDate);
 			}
 
 		});
